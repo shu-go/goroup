8dbd42f72e6eebee07286ea1d88f7834d156d344
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2018-04-16 16:55:29 +0900

    add goroutine util src goroutine.go

---
+++ goroutine.go
@@ -0,0 +1,42 @@
+package goroup
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Done returns receive-only chan that is sent when f() is done.
+func Done(f func()) <-chan struct{} {
+	doneChan := make(chan struct{})
+	go func() {
+		f()
+		doneChan <- struct{}{}
+	}()
+	return doneChan
+}
+
+func Select(chans ...interface{}) (c interface{}, value interface{}, recvOK bool) {
+	if len(chans) == 0 {
+		return nil, nil, false
+	}
+
+	var cases []reflect.SelectCase
+	for i, c := range chans {
+		cv := reflect.ValueOf(c)
+		if cv.Kind() != reflect.Chan || cv.Type().ChanDir()&reflect.RecvDir == 0 {
+			panic(fmt.Sprintf("%dth element is not a receivable chan (%v)", i, cv.Type().String()))
+		}
+
+		cases = append(cases, reflect.SelectCase{
+			Dir:  reflect.SelectRecv,
+			Chan: cv,
+		})
+	}
+
+	chosen, recv, recvOK := reflect.Select(cases)
+	if !recvOK {
+		return nil, nil, false
+	}
+
+	return chans[chosen], recv.Interface(), true
+}
