f82bf7d7ece78c68801800a66e26132a29d21743
Shuhei Kubota <kubota.shuhei@gmail.com>
2018-06-09 13:04:26 +0900

    fix test atomic

--- group_test.go
+++ group_test.go
@@ -2,6 +2,7 @@ package goroup_test
 
 import (
 	"context"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -11,29 +12,29 @@ import (
 
 func TestGroup(t *testing.T) {
 	t.Run("One", func(t *testing.T) {
-		result := 0
+		var result int64 = 0
 		f := func(c context.Context, params ...interface{}) {
 			gain := params[0].(int)
-			result += gain
+			atomic.AddInt64(&result, int64(gain))
 		}
 
 		pg := goroup.NewGroup()
 		pg.Add(goroup.Ready(f, 100))
 
-		gotwant.Test(t, result, 0)
+		gotwant.Test(t, atomic.LoadInt64(&result), int64(0))
 
 		g := pg.Go(nil)
 		g.Wait()
 
-		gotwant.Test(t, result, 100)
+		gotwant.Test(t, atomic.LoadInt64(&result), int64(100))
 	})
 
 	t.Run("Multiple", func(t *testing.T) {
-		result := 0
+		var result int64 = 0
 		f := func(c context.Context, params ...interface{}) {
 			gain := params[0].(int)
 			time.Sleep(time.Duration(gain) * time.Millisecond)
-			result += gain
+			atomic.AddInt64(&result, int64(gain))
 		}
 
 		pg := goroup.NewGroup()
@@ -41,27 +42,27 @@ func TestGroup(t *testing.T) {
 		pg.Add(goroup.Ready(f, 10))
 		pg.Add(goroup.Ready(f, 1))
 
-		gotwant.Test(t, result, 0)
+		gotwant.Test(t, atomic.LoadInt64(&result), int64(0))
 
 		g := pg.Go(nil)
 
 		g.WaitAny()
-		gotwant.TestExpr(t, result, result > 0)
+		gotwant.TestExpr(t, atomic.LoadInt64(&result), atomic.LoadInt64(&result) > 0)
 
 		g.PurgeDone()
 		g.WaitAny()
-		gotwant.TestExpr(t, result, result > 0)
+		gotwant.TestExpr(t, atomic.LoadInt64(&result), atomic.LoadInt64(&result) > 0)
 
 		g.Wait()
-		gotwant.Test(t, result, 111)
+		gotwant.Test(t, atomic.LoadInt64(&result), int64(111))
 	})
 
 	t.Run("Cancel", func(t *testing.T) {
-		result := 0
+		var result int64 = 0
 		f := func(c context.Context, params ...interface{}) {
 			gain := params[0].(int)
 			time.Sleep(time.Duration(gain) * time.Millisecond)
-			result += gain
+			atomic.AddInt64(&result, int64(gain))
 		}
 
 		pg := goroup.NewGroup()
@@ -69,19 +70,19 @@ func TestGroup(t *testing.T) {
 		pg.Add(goroup.Ready(f, 10))
 		pg.Add(goroup.Ready(f, 1))
 
-		gotwant.Test(t, result, 0)
+		gotwant.Test(t, atomic.LoadInt64(&result), int64(0))
 
 		g := pg.Go(nil)
 
 		g.WaitAny()
-		gotwant.TestExpr(t, result, result > 0)
+		gotwant.TestExpr(t, atomic.LoadInt64(&result), atomic.LoadInt64(&result) > 0)
 
 		g.PurgeDone()
 		g.WaitAny()
-		gotwant.TestExpr(t, result, result > 0)
+		gotwant.TestExpr(t, atomic.LoadInt64(&result), atomic.LoadInt64(&result) > 0)
 
 		g.Cancel()
 		g.Wait()
-		gotwant.Test(t, result, 11)
+		gotwant.Test(t, atomic.LoadInt64(&result), int64(11))
 	})
 }
